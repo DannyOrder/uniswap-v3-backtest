a87d7092d2753ad69c3ff371e7aa1dbff6844bc2
DannyOrder <915249+DannyOrder@users.noreply.github.com>
2023-08-01 17:10:08 +0800

    Update uniPoolData.mjs

--- uniPoolData.mjs
+++ uniPoolData.mjs
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import util from 'util';
 
 const urlForProtocol = (protocol) => {
   return protocol === 1 ? "https://api.thegraph.com/subgraphs/name/ianlapham/optimism-post-regenesis" : 
@@ -28,56 +29,63 @@ const requestBody = (request) => {
 
 }
 
-export const getPoolHourData = async (pool, fromdate, todate, protocol) => {
+export const getPoolHourData = async (pool, fromdate, todate, protocol, signal) => {
+  //console.log('getPoolHourData parameters:', {pool, fromdate, todate, protocol, signal});
 
   const query =  `query PoolHourDatas($pool: ID!, $fromdate: Int!, $todate: Int!) {
-  poolHourDatas ( where:{ pool:$pool, periodStartUnix_gt:$fromdate periodStartUnix_lt:$todate close_gt: 0}, orderBy:periodStartUnix, orderDirection:desc, first:1000) {
-    periodStartUnix
-    liquidity
-    high
-    low
-    pool {
-      id
-      totalValueLockedUSD
-      totalValueLockedToken1
-      totalValueLockedToken0
-      token0
-        {decimals}
-      token1
-        {decimals}
+    poolHourDatas ( where:{ pool:$pool, periodStartUnix_gt:$fromdate periodStartUnix_lt:$todate close_gt: 0}, orderBy:periodStartUnix, orderDirection:desc, first:1000) {
+      periodStartUnix
+      liquidity
+      high
+      low
+      pool {
+        id
+        totalValueLockedUSD
+        totalValueLockedToken1
+        totalValueLockedToken0
+        token0
+          {decimals}
+        token1
+          {decimals}
+      }
+      close
+      feeGrowthGlobal0X128
+      feeGrowthGlobal1X128
+      }
     }
-    close
-    feeGrowthGlobal0X128
-    feeGrowthGlobal1X128
-    }
-  }
-  `
+    `
 
   const url = urlForProtocol(protocol);
+  //console.log('protocol@58:', protocol);  // Add log
 
   try {
-    const response = await fetch(url, requestBody({query: query, variables: {pool: pool, fromdate: fromdate, todate} }));
-    const data = await response.json();
-
-    if (data && data.data && data.data.poolHourDatas) {
-     
-      return data.data.poolHourDatas;
-    }
-    else {
-      console.log("nothing returned from getPoolHourData")
+    const response = await fetch(url, requestBody({query: query, variables: {pool: pool, fromdate: fromdate, todate: todate}, signal: signal}));
+    let data;
+    try {
+      data = await response.json();
+    } catch (error) {
+      console.error('Error parsing response as JSON:', error);
       return null;
     }
 
-  } catch (error) {
+    if (data && data.data && data.data.poolHourDatas) {
+      //console.log('poolHourDatas:', util.inspect(data.data.poolHourDatas, {depth: null}));
+      return data.data.poolHourDatas;
+    }
+    else {
+      //console.log("nothing returned from getPoolHourData");
+      return null;
+    }
+} catch (error) {
+    console.error('Error:', error);
     return {error: error};
-  }
-
 }
-
+}
 
 export const poolById = async (id, protocol) => {
 
   const url = urlForProtocol(protocol);
+  //console.log('protocol@85:', protocol);  // Add log
 
   const poolQueryFields = `{
     id
@@ -119,15 +127,17 @@ export const poolById = async (id, protocol) => {
   }`
 
   try {
-
     const response = await fetch(url, requestBody({query: query, variables: {id: id}}));
     const data = await response.json();
 
+
     if (data && data.data) {
-      const pools = data.data;
- 
-      if (pools.id && pools.id.length && pools.id.length === 1) {
-        return pools.id[0]
+      const pools = data.data.id;
+      //console.log('pools@137:', pools);  // Add log
+      //console.log('pools.length@137:', pools.length);  // Add log
+    
+      if (pools && pools.length && pools.length === 1) {
+        return pools[0];
       }
     }
     else {
